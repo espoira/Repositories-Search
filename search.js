b71432ed1c35ebcaf4c40a7dcfd40dddf1b4fc04
espoira <79049883+espoira@users.noreply.github.com>
2023-03-18 01:06:47 +0300

    added file

---
+++ search.js
@@ -0,0 +1,24 @@
+async function searchRepos() {
+
+  let repos = [];
+  let subName = document.getElementById('repo-name').value;
+  
+  let repo = fetch(`https://api.github.com/search/repositories?q=${subName}`).then(
+    successResponse => {
+      if (successResponse.status != 200) {
+        return null;
+      } else {
+        return successResponse.json();
+      }
+    },
+    failResponse => {
+      return null;
+    }
+  );
+  repos.push(repo);
+  
+  let results = await Promise.all(repos);
+
+  return results;
+}
+
