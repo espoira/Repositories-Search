65a26f664f4968b79d4576e8d389a40331e0b254
espoira <79049883+espoira@users.noreply.github.com>
2023-03-19 19:53:05 +0300

    edited files

--- search.js
+++ search.js
@@ -1,29 +1,78 @@
-async function searchRepos() {
+'use strict';
+ 
+let form = document.querySelector('form');
+form.addEventListener('submit', formSubmit);
+ 
+ 
+function addRepository(data) {
 
-  let repos = [];
-  let subName = document.getElementById('repo-name').value;
+  document.querySelector('a').textContent = data.name;
+  document.querySelector('a').href = data.html_url;
   
-  let repo = fetch(`https://api.github.com/search/repositories?q=${subName}`).then(
-    successResponse => {
-      if (successResponse.status != 200) {
-        return null;
-      } else {
-        return successResponse.json();
-      }
-    },
-    failResponse => {
-      return null;
-    }
-  );
-  repos.push(repo);
-  
-  let results = await Promise.all(repos);
+  document.getElementById('owner').textContent = 'Владелец: ' + data.owner.login;
+  document.getElementById('description').innerHTML = (data.description) ?
+           '<b>Описание: </b>' + data.description : 'Нет описания';
 
-  return results;
+  document.querySelector('.repo-element').insertAdjacentHTML('afterend', 
+       '<div>' + document.querySelector('.repo-element').innerHTML + '</div>');  
+	   
+  document.querySelector('span').textContent = '';
+  
 }
 
-document.onkeydown = function(e) {
-  if (e.key == 'Enter') {
-    searchRepos();
+
+function formSubmit(e) {
+
+  e.preventDefault();
+  
+  let repos = [];
+  let message = 'Подождите, пожалуйста...';
+ 
+  let subName = document.getElementById('repo-name').value;
+  
+  if (!subName.trim()) {
+	  
+    message = 'Введите текст в строку поиска!';
+	
+  } else {
+
+    fetch(`https://api.github.com/search/repositories?q=${subName}`).then(
+
+      async (response) => {
+        if (response.status == 200) {
+          return response.json();
+        } else {
+          const data = await response.json();
+          message = 'Повторите позже';
+          return await Promise.reject(message);
+        }
+      }).then(
+	
+	  data => {
+		  
+        repos = data.items.slice(0, 10);
+       
+        if (repos.length != 0) {
+			
+          repos.forEach( 
+		    item => addRepository(item) 
+		  );
+		  
+        } else {
+			
+          message = 'Ничего не найдено';		  
+		}
+		
+        this.reset();
+		
+    }).catch(
+	
+	  err => {
+        message = err;
+		
+    });	
   }
+  
+  document.querySelector('span').textContent = message;
+  
 }
